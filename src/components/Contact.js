e127b6a66cdf4fe8454173c8f49fa169cf26b705
RosanioAccepto <matt.rosanio@acceptto.com>
2017-10-29 21:45:24 -0500

    Get state change working on input fields in contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,19 +14,31 @@ class Contact extends Component {
     };
 
     this.submitContactRequest = this.submitContactRequest.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   submitContactRequest(e) {
     e.preventDefault();
   }
 
+  handleChange(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
-    console.log("contact rendered");
     return (
       <div>
         <h3>Send us a message</h3>
         <form onSubmit={this.submitContactRequest} >
-
+          <input name="firstName" onChange={this.handleChange} />
+          <input name="lastName" onChange={this.handleChange} />
+          <input name="email" onChange={this.handleChange} />
+          <input name="phone" onChange={this.handleChange} />
+          <input name="message" onChange={this.handleChange} />
         </form>
       </div>
     );
